b9a30b7a830e2dc7d56de89459b4072c7e4b5454
vitor-sld <vitor.cteles@gmail.com>
2023-05-20 17:32:10 -0300

    adicionando ver~sao mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-questions]')
 
+    const hero = document.querySelector('.hero')
+    const alturaHero = hero.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY;
+        console.log(alturaHero)
+        if (posicaoAtual < alturaHero) {
+            ocultarElementosDoHeader();
+        } else {
+            exibirElementosDoHeader();
+        }
+    })
+
+    //Abas da section Shows
     for (i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -10,9 +25,27 @@ document.addEventListener('DOMContentLoaded', function () {
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
-
-
         })
+    }
+    //accordion da section FAQ
+    for (i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abrirOuFecharQuestion)
+    }
+
+    function ocultarElementosDoHeader() {
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden')
+    }
+
+    function exibirElementosDoHeader() {
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden')
+    }
+
+    function abrirOuFecharQuestion(elemento) {
+        const classeOpen = "faq__questions__item--is-open";
+        const elementoPai = elemento.target.parentNode;
+        elementoPai.classList.toggle(classeOpen)
     }
 
     function escondeTodasAbas() {
@@ -28,5 +61,8 @@ document.addEventListener('DOMContentLoaded', function () {
             buttons[i].classList.remove('shows__tabs__button--is-active');
         }
     }
+
+
+
 
 })
